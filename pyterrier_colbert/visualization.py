7db7404559b2541bce6e1147f0b275d6a0c6eacd
thorongil <officialantonio@outlook.it>
2022-01-08 12:51:14 +0100

    .

--- pyterrier_colbert/visualization.py
+++ pyterrier_colbert/visualization.py
@@ -37,19 +37,19 @@ class StaticPruningVisualization:
     def plot(self, x_value='reduction', y_value : str = 'nDCG@10', label_column='short-name'):
         assert len(self.dataframes) == len(self.names) and len(self.dataframes) != 0, 'names and dataframes must be of the same length'
         colors = self.colors[:len(self.dataframes)]
-        with plt.ioff():
-            fig, ax = plt.subplots(figsize=self.fig_size)
-            fig.suptitle(y_value + ' - comparison', fontsize=30)
-            ax.set_xlim([self.inf_x_limit, self.sup_x_limit])
-            for dataframe, name, color in zip(self.dataframes, self.names, colors):
-                dataframe = dataframe.sort_values(by=x_value)
-                ax.plot(dataframe[x_value], dataframe[y_value], marker='o', label=name, color=color)
-                if label_column and label_column in dataframe.columns:
-                    for i, text in enumerate(dataframe[label_column]):
-                        if dataframe.iloc[i][x_value] <= self.sup_x_limit:
-                            ax.text(dataframe.iloc[i][x_value] + .01, dataframe.iloc[i][y_value] + .01, text, fontsize=9)
-            ax.set_ylim([0, 1])
-            ax.set_xlabel(x_value, fontsize=self.font_size)
-            ax.set_ylabel(y_value, fontsize=self.font_size)
-            ax.legend(loc="best")
+        plt.ioff()
+        fig, ax = plt.subplots(figsize=self.fig_size)
+        fig.suptitle(y_value + ' - comparison', fontsize=30)
+        ax.set_xlim([self.inf_x_limit, self.sup_x_limit])
+        for dataframe, name, color in zip(self.dataframes, self.names, colors):
+            dataframe = dataframe.sort_values(by=x_value)
+            ax.plot(dataframe[x_value], dataframe[y_value], marker='o', label=name, color=color)
+            if label_column and label_column in dataframe.columns:
+                for i, text in enumerate(dataframe[label_column]):
+                    if dataframe.iloc[i][x_value] <= self.sup_x_limit:
+                        ax.text(dataframe.iloc[i][x_value] + .01, dataframe.iloc[i][y_value] + .01, text, fontsize=9)
+        ax.set_ylim([0, 1])
+        ax.set_xlabel(x_value, fontsize=self.font_size)
+        ax.set_ylabel(y_value, fontsize=self.font_size)
+        ax.legend(loc="best")
         return fig, ax
